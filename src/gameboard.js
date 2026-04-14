f088c196891d0fb03898d4fa423b89f789c6e12a
August Johnson <gurojohnson@gmail.com>
2026-04-13 20:15:56 -0400

    Feat: write placeShip(). Creeate ships and handle valid/invalid placements

--- src/gameboard.js
+++ src/gameboard.js
@@ -20,26 +20,48 @@ export class gameboard {
         }
 
         // place the ship in the right direction-- handle invalid placements
+        let validPosition = false;
         if (dir === 'lat') {
+            // check that it doesn't wrap rows
             for (let i = 0; i < rowEnds.length; i++) {
-                if (start < rowEnds[i] && (start + vessel.lives) <= rowEnds[i]) {
-                    for (let i = 0; i < vessel.lives; i++) {
-                        this.board[start + i].shipName = name;
-                       };
-                    };
+                if ((start + vessel.lives) <= rowEnds[i] && start > rowEnds[i - 1]) {
+                    validPosition = true;
+                    break;
                 };
             }
-        if (dir === 'vert') {
-            if (start + (vessel.lives * 10) < 100) {
-                for (let i = 0; i < vessel.lives; i += 10) {
+            // check that it doesn't collide
+            for (let i = 0; i < vessel.lives; i++) {
+                if (this.board[start + i].shipName) return validPosition = false;
+            }
+            // place ship
+            if (validPosition) {
+                for (let i = 0; i < vessel.lives; i++) {
                     this.board[start + i].shipName = name;
                 };
             };
+        };
+
+        if (dir === 'vert') {
+            if (start + (vessel.lives * 10) < 100) {
+                for (let i = 0; i < (vessel.lives * 10); i += 10) {
+                    if (this.board[start + i].shipName) {
+                        return validPosition = false;
+                    };
+                };
+                for (let i = 0; i < (vessel.lives * 10); i += 10) {
+                    this.board[start + i].shipName = name;
+                    };
+                };
+                return validPosition = true;
+            };
+            return validPosition;
         }
 
 
+        // need to handle collisions at each location
+
         // note for Claude: i stopped this function here and decided to get assistance from you before i went too far down a path that didn't feel right
-    }
+
     // note to self: needs to receiveHit() where coordinates are checked, and either missed shot is recorded or ship logs hit
 
     // note to self: function to check if ships have been sunk
