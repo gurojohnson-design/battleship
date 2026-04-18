5c49037f432d57fe1ef56f6229c3e9ad50dfc3dd
August Johnson <gurojohnson@gmail.com>
2026-04-18 15:01:34 -0400

    Transfer game logic from startGame.js to index.js and remove startGame.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
 import "./display/styles.css";
 import { player } from "./classes/player.js";
-import { ship } from "./classes/ship.js";
-import { gameboard } from "./classes/gameboard.js";
-import { startGame, startBtn } from "./display/display.js";
+import { gameDisplay, displayPlayerBoard, displayCpuBoard } from "./display.js";
 
-// we are now working on the DOM and game driver
+// create players-- place ships-- display boards
+// start button to kick off function
+const startBtn = document.createElement("button");
+startBtn.id = "startBtn";
+gameDisplay.append(startBtn);
+
+startBtn.addEventListener("click", startGame);
+
+function startGame() {
+  // make players
+  const user = new player("user", "player");
+  const cpu = new player("cpu", "cpu");
+
+  // display boards first for visual ship placement
+  displayPlayerBoard(user);
+  displayCpuBoard(cpu, user);
+
+  // place ships
+  // ship name, start index, direction 'lat' 'vert'
+  user.gameboard.placeShip("aircraft", 1, "lat");
+  user.gameboard.placeShip("battleship", 10, "lat");
+  user.gameboard.placeShip("cruiser", 20, "lat");
+  user.gameboard.placeShip("sub", 30, "lat");
+  user.gameboard.placeShip("destroyer", 40, "lat");
+
+  // place cpu ships
+  cpu.gameboard.placeShip("aircraft", 1, "lat");
+  cpu.gameboard.placeShip("battleship", 10, "lat");
+  cpu.gameboard.placeShip("cruiser", 20, "lat");
+  cpu.gameboard.placeShip("sub", 30, "lat");
+  cpu.gameboard.placeShip("destroyer", 40, "lat");
+
+  // display boards with ships
+  displayPlayerBoard(user);
+  displayCpuBoard(cpu, user);
+}
